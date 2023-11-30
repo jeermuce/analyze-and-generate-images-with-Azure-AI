9cfadac696862841451a3b981251956b06330423
jeermuce <ernesto_mc.dev@proton.me>
2023-11-30 15:06:37 -0600

    Revert changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-    const value = "World";
-
-    return (
-        <div>
-            <h1>Analyze or Generate image</h1>
-            <label htmlFor="image_url">Image URL</label>
-            <input type="text" id="image_url" />
-            <button>Analyze</button>
-            <button>Generate</button>
-        </div>
-    );
+  const value = 'World';
+  return <div>Hello {value}</div>;
 }
 
 export default App;
